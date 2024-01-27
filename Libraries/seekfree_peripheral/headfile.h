c9283832deed718d6fa5f7183840adfd6ef5456a
emm <101249805+emmismyname@users.noreply.github.com>
2024-01-27 16:52:07 +0800

    Add files via upload

---
+++ Libraries/seekfree_peripheral/headfile.h
@@ -0,0 +1,46 @@
+#ifndef __HEADFILE_H_
+#define __HEADFILE_H_
+
+
+
+
+#include "isr.h"
+#include <string.h>
+#include <stdio.h>
+#include <math.h>
+#include "intrins.h"
+//------STC32G SDK等
+#include "STC32Gxx.h"
+#include "board.h"
+#include "common.h"
+
+//------逐飞科技单片机外设驱动头文件
+#include "zf_uart.h"
+#include "zf_gpio.h"
+#include "zf_iic.h"
+#include "zf_adc.h"
+#include "zf_spi.h"
+#include "zf_tim.h"
+#include "zf_pwm.h"
+#include "zf_nvic.h"
+#include "zf_exti.h"
+#include "zf_delay.h"
+#include "zf_eeprom.h"
+
+//------逐飞科技产品驱动头文件
+#include "SEEKFREE_FONT.h"
+#include "SEEKFREE_18TFT.h"
+#include "SEEKFREE_IPS114_SPI.h"
+#include "SEEKFREE_MPU6050.h"
+#include "SEEKFREE_ABSOLUTE_ENCODER.h"
+#include "SEEKFREE_WIRELESS.h"
+#include "SEEKFREE_PRINTF.h"
+#include "SEEKFREE_AT24C02.h"
+#include "SEEKFREE_BLUETOOTH_CH9141.h"
+#include "SEEKFREE_CONFIG.h"
+#include "SEEKFREE_IMU660RA.h"
+#include "SEEKFREE_DL1A.h"
+//自定义
+#include "MYCODE.h"
+
+#endif
