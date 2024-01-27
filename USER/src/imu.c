084ec435a1dd09817d359024d29269fe624eac1c
emm <101249805+emmismyname@users.noreply.github.com>
2024-01-27 15:38:54 +0800

    Add files via upload

---
+++ USER/src/imu.c
@@ -0,0 +1,154 @@
+#include "imu.h"
+
+int16 gyro_flag = 0; // 0为零漂状态，1为正在计算标志，2获取但不计算  3获取数值并积分
+float q0 = 1.0f, q1 = 0.0f, q2 = 0.0f, q3 = 0.0f;
+float pitch = 0.0f, roll = 0.0f, yaw = 0.0f;
+float pitch_last = 0.0f, roll_last = 0.0f, yaw_last = 0.0f;
+float pitch_err = 0.0f, roll_err = 0.0f, yaw_err = 0.0f;
+float pitch_circle = 0.0f, roll_circle = 0.0f, yaw_circle = 0.0f;
+float pitch_result = 0.0f, roll_result = 0.0f, yaw_result = 0.0f;
+static int first_flag = 0;
+
+int Zero_Offset_Count = 0; // 初始化累加值
+float move_z_gyro;
+float y_gyro = 0, z_gyro = 0, x_gyro = 0;			  // 初始化陀螺仪数值
+float y_gyro_offset, z_gyro_offset, x_gyro_offset;	  // 初始化陀螺仪零漂数值
+float y_acc, z_acc, x_acc;							  // 初始化加速度计数值
+float y_acc_offset, z_acc_offset, x_acc_offset;		  // 初始化陀螺仪零漂数值
+float dt = 0.01f;									  // 0.01系数
+int Zero_Offset_End;								  // 100次
+float det_yaw = 0;									  // yaw的变化数值
+float yaw_output = 0;								  // 输出值的上一状态数值
+float yaw_result_last = 0, org_yaw = 0, gyro_yaw = 0; // 输出值的上一状态数值
+float gyro[11] = {0}, gyro_sum;
+//-------------------------------------------------------------------------------------------------------------------
+// @brief		陀螺仪均值滤波
+//
+//-------------------------------------------------------------------------------------------------------------------
+// float gyro_aquire(float org_gyro)
+// {
+// 	static int i = 0;
+// 	gyro[1] = org_gyro;
+// 	for (i = 1; i <=10; i++)
+// 	{
+// 		gyro[i+1] = gyro[i];
+// 	}
+// 	for (i = 1; i <=10; i++)
+// 	{
+// 		gyro_sum += gyro[i];
+// 	}
+// 	gyro[11] = gyro_sum / 10;
+// 	gyro_sum = 0;
+// 	return gyro[11];
+// }
+
+//-------------------------------------------------------------------------------------------------------------------
+//  @brief      MPU数据获取
+//  @param
+//  @return
+//-------------------------------------------------------------------------------------------------------------------
+void MPU_Acquire()
+{
+	MPU6050_Refresh_DMP();
+	MPU_Get_Gyroscope(&mpu6050_gyro_x, &mpu6050_gyro_y, &mpu6050_gyro_z);
+}
+
+//-------------------------------------------------------------------------------------------------------------------
+//  @brief      MPU数据计算
+//  @param
+//  @return
+//-------------------------------------------------------------------------------------------------------------------
+void MPU_Process()
+{
+	// z_gyro=mpu6050_gyro_z-z_gyro_offset;
+	z_gyro = mpu6050_gyro_z;
+	gyro_yaw += z_gyro;
+	yaw_last = yaw;
+	yaw_result_last = yaw_result;
+	q0 = Q[0] / q30;
+	q1 = Q[1] / q30;
+	q2 = Q[2] / q30;
+	q3 = Q[3] / q30;																	// 四元数
+	// roll = asin(-2 * q1 * q3 + 2 * q0 * q2) * 57.3;										// 计算出横滚角
+	// pitch = atan2(2 * q2 * q3 + 2 * q0 * q1, -2 * q1 * q1 - 2 * q2 * q2 + 1) * 57.3;	// 计算出俯仰角
+	// yaw = atan2(2 * (q1 * q2 + q0 * q3), q0 * q0 + q1 * q1 - q2 * q2 - q3 * q3) * 57.3; // 计算出偏航角
+	pitch = asin(-2 * Q[1] * Q[3] + 2 * Q[0] * Q[2]) * 57.3;													// pitch
+	roll = atan2(2 * Q[2] * Q[3] + 2 * Q[0] * Q[1], -2 * Q[1] * Q[1] - 2 * Q[2] * Q[2] + 1) * 57.3;				// roll
+	yaw = atan2(2 * (Q[1] * Q[2] + Q[0] * Q[3]), Q[0] * Q[0] + Q[1] * Q[1] - Q[2] * Q[2] - Q[3] * Q[3]) * 57.3; // yaw
+	// if (pitch < 0)
+	// 	pitch += 360;
+	// if (roll < 0)
+	// 	roll += 360;
+	org_yaw = yaw;
+	if (yaw < 0)
+		yaw += 360;
+	if (first_flag == 0)
+	{
+		pitch_last = pitch;
+		roll_last = roll;
+		yaw_last = yaw;
+		first_flag++;
+	}
+	// pitch_err = pitch - pitch_last;
+	// roll_err = roll - roll_last;
+	yaw_err = yaw - yaw_last;
+	// if (pitch_err < -180)
+	// 	pitch_circle++;
+	// else if (pitch_err > 180)
+	// 	pitch_circle--;
+	// if (roll_err < -180)
+	// 	roll_circle++;
+	// else if (roll_err > 180)
+	// 	roll_circle--;
+	if (yaw_err < -180)
+		yaw_circle++;
+	else if (yaw_err > 180)
+		yaw_circle--;
+	// pitch_last = pitch;
+	// roll_last = roll;
+	// pitch_result = pitch + 360 * pitch_circle;
+	// roll_result = roll + 360 * roll_circle;
+	yaw_result = yaw + 360 * yaw_circle;
+	det_yaw = yaw_result - yaw_result_last;
+	if (myabs(det_yaw) < 100)
+		yaw_output += det_yaw;
+}
+
+//-------------------------------------------------------------------------------------------------------------------
+//  @brief      初始化读取积分零飘
+//  @param      none
+//  @param
+//  @return     none
+//-------------------------------------------------------------------------------------------------------------------
+// void ZeroOffset_init()
+// {
+// 	while(Zero_Offset_Count < Zero_Offset_End)
+// 	{
+// 		MPU_Get_Gyroscope(&mpu6050_gyro_x,&mpu6050_gyro_y, &mpu6050_gyro_z);
+// 		z_gyro_offset+=z_gyro*dt;
+// 		wireless_send(4,5);
+// 		Zero_Offset_Count++;
+// 	}
+// 	ips114_clear(WHITE);
+// }
+
+//-------------------------------------------------------------------------------------------------------------------
+//  @brief      陀螺仪dmp解算自动纠正时间
+//  @param      none
+//  @param
+//  @return     none
+//-------------------------------------------------------------------------------------------------------------------
+void Zero_Offset_set()
+{
+	static int i = 1;
+	while (i < Zero_Offset_time)
+	{
+		MPU6050_Refresh_DMP();
+		pitch = asin(-2 * Q[1] * Q[3] + 2 * Q[0] * Q[2]) * 57.3;													// pitch
+		roll = atan2(2 * Q[2] * Q[3] + 2 * Q[0] * Q[1], -2 * Q[1] * Q[1] - 2 * Q[2] * Q[2] + 1) * 57.3;				// roll
+		yaw = atan2(2 * (Q[1] * Q[2] + Q[0] * Q[3]), Q[0] * Q[0] + Q[1] * Q[1] - Q[2] * Q[2] - Q[3] * Q[3]) * 57.3; // yaw
+		i++;
+		delay_ms(5);
+	}
+	gyro_flag = 1; // 开启陀螺仪使用
+} // 初始化纠正陀螺仪
---
+++ USER/src/motor.c
@@ -0,0 +1,412 @@
+#include "motor.h"
+
+uint8 brusheless_flag;	// 无刷开关
+uint16 brusheless_duty; // 无刷电机数值数值范围在500-1000对应占空比为0%-100%
+uint8 motor_flag;
+
+uint8 Dir_L = 1;   // 1左边方向正转  0反转
+uint8 Dir_R = 0;   // 0右边方向正转  1反转
+uint8 Dir_car = 1; // 正走dir_car=1 L=1 R=0  ;dir_car=0 反走 L=0 R=1
+
+int16 templ_pluse = 0;
+int16 tempr_pluse = 0;
+
+float Target_Speed_L = 0; // 左轮目标速度
+float Target_Speed_R = 0; // 右轮目标速度
+
+float Current_Speed_L; // 左轮当前速度
+float Current_Speed_R; // 右轮当前速度
+
+float duty_L;	  // 左电机pid占空比
+float duty_R;	  // 右电机pid占空比
+float set_duty_L; // 左电机输出占空比
+float set_duty_R; // 右电机输出占空比
+
+int32 distance_L = 0; // 左轮行驶距离
+int32 distance_R = 0; // 右轮行驶距离
+
+PID_P Swerve_PID;	   // 转弯差速pid
+PID_P Forward_PID;	   // 直行PID控制
+PID_P Pout_PID;		   // 陀螺仪PID控制
+PID_P Pout_rate_PID; //角速度PID控制
+PID_P roundaboutL_PID; // 左环岛pid
+PID_P roundaboutR_PID; // 右环岛pid
+PID_P Speed_PID_L;	   // 左轮速度pid
+PID_P Speed_PID_R;	   // 右轮速度pid
+PID_P climb_PID;				//上坡pid
+int templ_pluse;	  // 左轮脉冲个数
+int tempr_pluse;	  // 右轮脉冲个数
+int Target_speed = 0; // 车体目标速度
+
+// float PIDOUT_speed_L;			//左轮速度环的偏差计算结果
+// float PIDOUT_speed_R;			//右轮速度环的偏差计算结果
+
+// int32 Target_pluse_L=0;			//左轮目标脉冲数目
+// int32 Target_pluse_R=0;			//右轮目标脉冲数目
+
+// int32 Motor_pluse_L;			//左轮速度环的脉冲个数
+// int32 Motor_pluse_R;			//左轮速度环的脉冲个数
+
+// float Motor_duty_L=470;			//左电机目标初始占空比用于克服死区电压，电源为满电状态
+// float Motor_duty_R=360;			//右电机目标初始占空比用于克服死区电压，电源为满电状态
+
+//-------------------------------------------------------------------------------------------------------------------
+//  @brief      pid控制器输出更新内部
+//  @param      pid				pid参数
+//  @param
+//  @return     *pid参数 内部输出结果清零
+//-------------------------------------------------------------------------------------------------------------------
+void pid_init(PID_P *pid) // PID的误差输出初始化
+{
+	pid->last_err = 0;
+	pid->lastBefore_err = 0;
+	pid->err = 0;
+	pid->pid_out = 0;
+	pid->i_out = 0;
+	pid->d_out = 0;
+	pid->p_out = 0;
+}
+
+//-------------------------------------------------------------------------------------------------------------------
+//  @brief      pid位置式控制器输出
+//  @param      pid				pid参数
+//  @param      error			pid输入误差
+//  @return     PID输出结果
+//-------------------------------------------------------------------------------------------------------------------
+float PID_Loc_Control(PID_P *pid, float error)
+{
+	if (error > 10 && error < -10)
+	{
+		pid->i_out += pid->KI * error;
+	} // 位置式PID积分项累加
+	else if (error < 10 && error > -10)
+	{
+		pid->i_out = 0;
+	}
+	pid->p_out = pid->KP * error;
+	pid->d_out = pid->KD * (error - pid->last_err);
+	pid->pid_out = pid->i_out + pid->p_out + pid->d_out;
+
+	// PID输出限幅
+	if (pid->pid_out > (600))
+		pid->pid_out = 600;
+	if (pid->pid_out < -(600))
+		pid->pid_out = -(600);
+	// 更新误差,用于下次计算
+	pid->last_err = error;
+	// 返回PID输出值
+	return pid->pid_out;
+}
+
+//-------------------------------------------------------------------------------------------------------------------
+//  @brief      pid位置式控制器输出
+//  @param      pid				pid参数
+//  @param      error			pid输入误差
+//  @return     PID输出结果
+//-------------------------------------------------------------------------------------------------------------------
+float PID_Loc_direction_Control(PID_P *pid, float error)
+{
+	if (pid->err > 4 && pid->err < -4)
+	{
+		pid->i_out = 0;
+	} // 位置式PID积分项累加
+	else if (pid->err < 4 && pid->err > -4)
+	{
+		pid->i_out += pid->KI * error;
+	}
+
+	// pid->i_out += pid->KI * error;	   //位置式PID积分项累加
+
+	if (pid->i_out > (100))
+		pid->i_out = 100;
+	if (pid->i_out < -(100))
+		pid->i_out = -(100);
+	pid->p_out = pid->KP * error;
+	pid->d_out = pid->KD * (error - pid->last_err);
+	pid->pid_out = pid->i_out + pid->p_out + pid->d_out;
+
+	// PID输出限幅
+	if (pid->pid_out > (600))
+		pid->pid_out = 600;
+	if (pid->pid_out < -(600))
+		pid->pid_out = -(600);
+	// 更新误差,用于下次计算
+	pid->last_err = error;
+	// 返回PID输出值
+	return pid->pid_out;
+}
+
+//-------------------------------------------------------------------------------------------------------------------
+//  @brief      pid位置式控制器输出
+//  @param      pid				pid参数
+//  @param      error			pid输入误差
+//  @return     PID输出结果
+//-------------------------------------------------------------------------------------------------------------------
+float PID_Loc_angle_Control(PID_P *pid, float error)
+{
+	if (pid->err > 4 && pid->err < -4)
+	{
+		pid->i_out = 0;
+	} // 位置式PID积分项累加
+	else if (pid->err < 5 && pid->err > -5)
+	{
+		pid->i_out += pid->KI * error;
+	}
+
+	// pid->i_out += pid->KI * error;	   //位置式PID积分项累加
+
+	if (pid->i_out > (1000))
+		pid->i_out = 1000;
+	if (pid->i_out < -(1000))
+		pid->i_out = -(1000);
+	pid->p_out = pid->KP * error;
+	pid->d_out = pid->KD * (error - pid->last_err);
+	pid->pid_out = pid->i_out + pid->p_out + pid->d_out;
+
+	// PID输出限幅
+	if (pid->pid_out > (100000))
+		pid->pid_out = 100000;
+	if (pid->pid_out < -(100000))
+		pid->pid_out = -(100000);
+	// 更新误差,用于下次计算
+	pid->last_err = error;
+	// 返回PID输出值
+	return pid->pid_out;
+}
+
+//-------------------------------------------------------------------------------------------------------------------
+//  @brief      pid位置式控制器输出
+//  @param      pid				pid参数
+//  @param      error			pid输入误差
+//  @return     PID输出结果
+//-------------------------------------------------------------------------------------------------------------------
+float PID_Loc_angle_rate_Control(PID_P *pid, float error)
+{
+	if (pid->err > 10 && pid->err < -10)
+	{
+		pid->i_out = 0;
+	} // 位置式PID积分项累加
+	else if (pid->err < 10 && pid->err > -10)
+	{
+		pid->i_out += pid->KI * error;
+	}
+
+	// pid->i_out += pid->KI * error;	   //位置式PID积分项累加
+
+	if (pid->i_out > (200))
+		pid->i_out = 200;
+	if (pid->i_out < -(200))
+		pid->i_out = -(200);
+	pid->p_out = pid->KP * error;
+	pid->d_out = pid->KD * (error - pid->last_err);
+	pid->pid_out = pid->i_out + pid->p_out + pid->d_out;
+
+	// PID输出限幅
+	if (pid->pid_out > (600))
+		pid->pid_out = 600;
+	if (pid->pid_out < -(600))
+		pid->pid_out = -(600);
+	// 更新误差,用于下次计算
+	pid->last_err = error;
+	// 返回PID输出值
+	return pid->pid_out;
+}
+
+
+//-------------------------------------------------------------------------------------------------------------------
+//  @brief      pid增量式控制器输出
+//  @param      pid				pid参数
+//  @param      error			pid输入误差
+//  @return     PID输出结果
+//-------------------------------------------------------------------------------------------------------------------
+float PID_Inc_Control(PID_P *pid, float error)
+{
+	pid->p_out = pid->KP * (error - pid->last_err);
+	pid->i_out = pid->KI * error;
+	pid->d_out = pid->KD * (error - 2 * pid->last_err + pid->lastBefore_err);
+
+	pid->lastBefore_err = pid->last_err;
+	pid->last_err = error;
+
+	pid->pid_out += pid->p_out + pid->i_out + pid->d_out;
+	return pid->pid_out;
+}
+
+
+//-------------------------------------------------------------------------------------------------------------------
+//  @brief      pid位置式控制器输出
+//  @param      pid				pid参数
+//  @param      error			pid输入误差
+//  @return     PID输出结果
+//-------------------------------------------------------------------------------------------------------------------
+float PID_Loc_motor_Control(PID_P *pid, float error)
+{
+	pid->i_out += pid->KI * error; // 位置式PID积分项累加
+	pid->p_out = pid->KP * error;
+	pid->d_out = pid->KD * (error - pid->last_err);
+	pid->pid_out = pid->i_out + pid->p_out + pid->d_out;
+
+	// PID输出限幅
+	if (pid->pid_out > 7500)
+		pid->pid_out = 8500;
+	if (pid->pid_out < -7500)
+		pid->pid_out = -8500;
+	// 更新误差,用于下次计算
+	pid->last_err = error;
+	// 返回PID输出值
+	return pid->pid_out;
+}
+
+
+//-------------------------------------------------------------------------------------------------------------------
+//  @brief      无线转串口发送
+//  @param
+//  @param
+//  @return
+//-------------------------------------------------------------------------------------------------------------------
+void counte_quire() // 获得编码器脉冲个数
+{
+	// 读取采集到的编码器脉冲数
+	templ_pluse = ctimer_count_read(SPEEDL_PLUSE);
+	tempr_pluse = ctimer_count_read(SPEEDR_PLUSE);
+
+	// 计数器清零
+	ctimer_count_clean(SPEEDL_PLUSE);
+	ctimer_count_clean(SPEEDR_PLUSE);
+
+	// 采集方向信息
+	if (0 == SPEEDL_DIR)
+	{
+		templ_pluse = -templ_pluse;
+	}
+
+	if (1 == SPEEDR_DIR)
+	{
+		tempr_pluse = -tempr_pluse;
+	}
+
+	Current_Speed_R = 1.859375 * tempr_pluse; // 30/68 * 6.8 * 3.14 * n/1024 /0.005
+	Current_Speed_L = 1.859375 * templ_pluse;
+	distance_L += Current_Speed_L;
+	distance_R += Current_Speed_R;
+}
+
+//-------------------------------------------------------------------------------------------------------------------
+//  @brief      电调控制是看高电平时间，范围： 1ms-2ms
+//  @param      计算无刷电调转速   （1ms - 2ms）/20ms * 10000（10000是PWM的满占空比时候的值）	1ms 为 0%			 2ms 为 100%
+//  @param      brushless_duty无刷电调转速 100% 为 1000  无刷电调转速 0%   为 500
+//  @return     修改brushless_duty(全局变量，不内置函数)的值，可以修改无刷电调转速
+//-------------------------------------------------------------------------------------------------------------------
+void brushes_out()
+{
+	if (brusheless_flag == 1)
+	{
+		if (brusheless_duty < 500)
+			brusheless_duty = 500;
+		else if (brusheless_duty > 1000)
+			brusheless_duty = 1000;
+		// 限制幅度
+
+		// 控制无刷电调转速
+		pwm_duty(PWMB_CH4_P77, brusheless_duty);
+		pwm_duty(PWMB_CH3_P33, brusheless_duty + 10); // 左右电机有一定不同
+	}
+	else if (brusheless_flag == 0)
+	{
+		brusheless_duty = 500;
+		pwm_duty(PWMB_CH4_P77, 0);
+		pwm_duty(PWMB_CH3_P33, 0); // 左右电机有一定不同
+	}
+}
+
+
+void motor_output() // 电机PWM输出
+{
+	if (motor_flag == 0)
+	{
+		set_duty_L = 0; // 左右电机不输出
+		set_duty_R = 0; // 左右电机不输出
+	}
+	else if (motor_flag == 1)
+	{
+		Speed_PID_L.err = Target_Speed_L - Current_Speed_L;
+		Speed_PID_R.err = Target_Speed_R - Current_Speed_R;
+		duty_L = PID_Loc_motor_Control(&Speed_PID_L, Speed_PID_L.err); // 目标基础速度+转向速度
+		duty_R = PID_Loc_motor_Control(&Speed_PID_R, Speed_PID_R.err); // 目标基础速度+转向速度
+
+		// 积分限制幅度
+		if (Speed_PID_L.i_out > 3000)
+		{
+			Speed_PID_L.i_out = 3000;
+		}
+		else if (Speed_PID_L.i_out < -3000)
+		{
+			Speed_PID_L.i_out = -3000;
+		}
+		if (Speed_PID_R.i_out > 3000)
+		{
+			Speed_PID_R.i_out = 3000;
+		}
+		else if (Speed_PID_R.i_out < -3000)
+		{
+			Speed_PID_R.i_out = -3000;
+		}
+
+		if (duty_L >= 0)
+		{
+			Dir_L = 1;
+			set_duty_L = duty_L;
+			if (set_duty_L >= 6500)
+				set_duty_L = 6500;
+			else if (set_duty_L <= 0)
+				set_duty_L = 0;
+		}
+		else if (duty_L < 0)
+		{
+			Dir_L = 0;
+			set_duty_L = -duty_L;
+			if (set_duty_L >= 6500)
+				set_duty_L = 6500;
+			else if (set_duty_L <= 0)
+				set_duty_L = 0;
+		}
+		if (duty_R >= 0)
+		{
+			set_duty_R = duty_R;
+			Dir_R = 0;
+			if (set_duty_R >= 6500)
+				set_duty_R = 6500;
+			else if (set_duty_R <= 0)
+				set_duty_R = 0;
+		}
+		else if (duty_R < 0)
+		{
+			Dir_R = 1;
+			set_duty_R = -duty_R;
+			if (set_duty_R >= 6500)
+				set_duty_R = 6500;
+			else if (set_duty_R <= 0)
+				set_duty_R = 0;
+		}
+
+		// if(Dir_car==1)
+		// {
+		// 	Dir_L=1;
+		// 	Dir_R=0;
+		// }
+		// else if(Dir_car==0)
+		// { 0
+		// 	Dir_L=0;
+		// 	Dir_R=1;
+		// }
+		// //限制幅度输出
+		// if(duty_L >= 6500) duty_L = 6500;
+		// else if(duty_L <= 0) duty_L = 0;
+		// if(duty_R >= 6500) duty_R = 6500;
+		// else if(duty_R <= 0) duty_R = 0;
+	}
+	DIR_1 = Dir_L;
+	DIR_2 = Dir_R;
+	pwm_duty(PWM_1, set_duty_L);
+	pwm_duty(PWM_2, set_duty_R);
+}
